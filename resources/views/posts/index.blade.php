0576332183e178feeeae1bf1cf38951d4b46e2ce
diegocasillas <diegocasillasdev@gmail.com>
2018-10-22 12:24:08 +0300

    Add index posts view

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,23 @@
+@extends('master')
+
+@section('main')
+    <div class="row">
+        <section class="col-lg-10 mx-auto">
+            @forelse ($posts as $post)
+                @post([
+                    'title' => $post->title,
+                    'category' => $post->category,
+                    'body' => $post->body,
+                    'url' => $post->url
+                ])
+                @endpost
+
+                @if (!$loop->last)
+                    <hr>
+                @endif
+            @empty
+                <div class="h3 text-center">There aren't posts</div>
+            @endforelse
+        </section>
+    </div>
+@endsection
