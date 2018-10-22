37a546afaa065739474e2c7c0145be0cc5bc71c0
diegocasillas <diegocasillasdev@gmail.com>
2018-10-22 12:22:52 +0300

    Add master layout

---
+++ resources/views/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    @include('partials.head')
+    <body>
+        @include('partials.nav')
+
+        @include('partials.header')
+
+        <main class="container mt-5 mb-5">
+            @yield('main')
+        </main>
+
+        @include('partials.footer')
+
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
