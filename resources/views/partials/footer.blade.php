089fb811165ee5b7fcbd0dbf08b63a41ff9b7bfa
diegocasillas <diegocasillasdev@gmail.com>
2018-10-22 12:23:07 +0300

    Add partial views

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="text-muted text-center mb-5">
+    <div>
+        Designed with <span style="color: #d81313;">&#10084;</span> by <a href="https://github.com/diegocasillas">@diegocasillas</a>
+    </div>
+    <div>
+        &copy; 2016-{{ date('Y') }} - All rights reserved
+    </div>
+</footer>
