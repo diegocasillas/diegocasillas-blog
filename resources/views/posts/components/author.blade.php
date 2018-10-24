9e0a043b0ddf14586358c951617f621cc42bb6ae
diegocasillas <diegocasillasdev@gmail.com>
2018-10-24 23:08:00 +0300

    Add author view component

---
+++ resources/views/posts/components/author.blade.php
@@ -0,0 +1,25 @@
+<div class="p-5 col-md-10 mx-auto mt-5 author-info soft-shadow" style="box-shadow: 0px 2px 10px lightgrey;">
+    <div class="row">
+        <div class="text-center col-md-8">
+            <h5>Diego Casillas</h5>
+
+            <hr>
+
+            <p>I am a Web Application Developer. I was bored so I created this blog to share stuff about programming.</p>
+
+            <a href="https://twitter.com/diego_casillas_" target="_blank">Twitter</a>
+
+            <span>|</span>
+
+            <a href="https://github.com/diegocasillas" target="_blank">GitHub</a>
+
+            <span>|</span>
+
+            <a href="https://www.linkedin.com/in/diegocasillas" target="_blank">LinkedIn</a>
+        </div>
+
+        <div class="col-6 col-md-3 mx-auto">
+            <img class="author-picture img-fluid" src="https://secure.gravatar.com/avatar/ff9c60e52c9822d8b71e32e4de437b3b?s=300&amp;d=mm&amp;r=g" alt="Author image">
+        </div>
+    </div>
+</div>
